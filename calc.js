1d9c76560320e01ae06449a0a91a66619595bf3d
ChaseFaggard <93607164+ChaseFaggard@users.noreply.github.com>
2021-11-29 13:30:39 -0600

    Add files via upload

--- calc.js
+++ calc.js
@@ -6,27 +6,71 @@ var output = document.querySelector('.output');
   
   Figure out how to set the values of num1 and num2 as the buttons are clicked
   */
+var num1;
+var num2;
+var Operators;
+(function (Operators) {
+    Operators[Operators["ADD"] = 0] = "ADD";
+    Operators[Operators["SUBTRACT"] = 1] = "SUBTRACT";
+    Operators[Operators["MULTIPLY"] = 2] = "MULTIPLY";
+    Operators[Operators["DIVIDE"] = 3] = "DIVIDE";
+})(Operators || (Operators = {}));
+var operator;
 document.querySelector('.calc').addEventListener('click', function (evt) {
     var button = evt.target;
     if (button.className.indexOf('num') !== -1) {
         console.log(button.innerHTML);
         output.value += button.innerHTML;
+        if (operator == undefined)
+            num1 = Number(button.innerHTML);
+        else
+            num2 = Number(button.innerHTML);
     }
     if (button.className.indexOf('operator') !== -1) {
         console.log(button.innerHTML);
         output.value += button.innerHTML;
+        switch (button.innerHTML) {
+            case '+':
+                operator = Operators.ADD;
+                break;
+            case '-':
+                operator = Operators.SUBTRACT;
+                break;
+            case '*':
+                operator = Operators.MULTIPLY;
+                break;
+            case '/':
+                operator = Operators.DIVIDE;
+                break;
+        }
     }
     if (button.className.indexOf('equal') !== -1) {
         console.log(button.innerHTML);
-        var calculation = calculate( /*pass in params here*/);
+        var calculation = calculate(num1, num2, operator);
         output.value = calculation.toString();
+        num1 = calculation;
     }
+    output.value += ' ';
     /* create another condition to clear the value of the input when C is pressed*/
+    if (button.className.indexOf('reset') !== -1) {
+        output.value = '';
+        num1 = undefined;
+        num2 = undefined;
+        operator = undefined;
+    }
 });
 //create a function that takes in 2 numbers and a string (operand)
-function calculate( /*You fill in here*/) {
-    /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
-    return that value
-    */
-    return 0;
+function calculate(num1, num2, operator) {
+    if (num1 == undefined || num2 == undefined || operator == undefined)
+        return 0;
+    switch (operator) {
+        case Operators.ADD:
+            return num1 + num2;
+        case Operators.SUBTRACT:
+            return num1 - num2;
+        case Operators.MULTIPLY:
+            return num1 * num2;
+        case Operators.DIVIDE:
+            return num1 / num2;
+    }
 }
--- calc.ts
+++ calc.ts
@@ -6,30 +6,68 @@ let output = document.querySelector('.output') as HTMLInputElement;
     
     Figure out how to set the values of num1 and num2 as the buttons are clicked
     */
+enum Operators{ ADD, SUBTRACT, MULTIPLY, DIVIDE }
+
+let num1: number
+let num2: number
+let operator: Operators
+
 document.querySelector('.calc').addEventListener('click',(evt)=>{
     let button = evt.target as HTMLButtonElement;
   
     if(button.className.indexOf('num')!==-1){
         console.log(button.innerHTML);      
         output.value += button.innerHTML;
+        if(operator == undefined) num1 = Number(button.innerHTML)
+        else num2 = Number(button.innerHTML)
     }
     if(button.className.indexOf('operator')!==-1){
         console.log(button.innerHTML);      
         output.value += button.innerHTML;
+        switch(button.innerHTML) {
+            case '+':
+                operator = Operators.ADD
+                break
+            case '-': 
+                operator = Operators.SUBTRACT
+                break
+            case '*':
+                operator = Operators.MULTIPLY
+                break
+            case '/':
+                operator = Operators.DIVIDE
+                break
+        }
     }
     if(button.className.indexOf('equal')!==-1){
         console.log(button.innerHTML);
-        let calculation = calculate(/*pass in params here*/);
+        const calculation = calculate(num1, num2, operator);
         output.value = calculation.toString();
+        num1 = calculation
     }
+
+    output.value += ' '
+
     /* create another condition to clear the value of the input when C is pressed*/
+    if(button.className.indexOf('reset')!==-1) {
+        output.value = ''
+        num1 = undefined
+        num2 = undefined
+        operator = undefined
+    }
 });
 
 //create a function that takes in 2 numbers and a string (operand)
-function calculate(/*You fill in here*/){
-    /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
-    return that value
-    */
-   return 0;
-
+function calculate(num1: number, num2: number, operator: Operators): number {
+    if(num1 == undefined || num2 == undefined || operator == undefined) return 0
+    switch(operator) {
+        case Operators.ADD:
+            return num1 + num2
+        case Operators.SUBTRACT:
+            return num1 - num2
+        case Operators.MULTIPLY:
+            return num1 * num2
+        case Operators.DIVIDE:
+            return num1 / num2
+    }
 }
